39bdf66dcfa9b3aab606d13f19a711c0b6bcb497
Asyedibrahim <syedibrahim7252@gmail.com>
2024-10-28 16:17:25 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -16,7 +16,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 4000;
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
+const PORT = 3004;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} !!!`);
 1});
